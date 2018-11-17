20fc6ae619083870eec8ae8e860279b80b240988
Brandon Squires <squiresbrandon@yahoo.com>
2018-11-17 09:45:18 -0700

    nic broke things

--- pokeSite/pokedex/views.py
+++ pokeSite/pokedex/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 import requests
 
@@ -6,21 +6,25 @@ from .models import pokemon
 
 
 def index(request):
+    pokeList = pokemon.objects.all().order_by('getName')
+    return render(request, 'pokedex/index.html',{
+        'pokeList': "pokelist",
+    })
 
-    return HttpResponse("hi")
-
-pokeList = pokemon.objects.all().order_by('getName')
+def init(request):
     response = requests.get('https://pokeapi.co/api/v2/pokemon/')
-    for i in range(1,151):
-        allPokeData = response.json()
-        response = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(i) + '/')
+    allPokeData = response.json()
+    for i in range(0,1):
+        name = allPokeData['results'][i]['name']
+        response = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(i+1) + '/')
         pokeData = response.json()
+    
         if len(pokeData['types']) == 2:
             type1 = pokeData['types'][0]['type']['name']
             type2 = pokeData['types'][1]['type']['name']
         else:
             type1 = pokeData['types'][0]['type']['name']
             type2 = "none"
-        p[i] = pokemon(pokeName = allPokeData['results'][i]['name'], pokeType1 = type1, pokeType2 = type2)
+        p = pokemon(pokeName = allPokeData['results'][i]['name'], pokeType1 = type1, pokeType2 = type2)
 
-    return render(request, 'pokedex/index.html')
+    return redirect('pokedex:index')    
