c14f5066278c7f8511cd7cb25a194533e15bc11a
ryanjohnson <ryan.johnson.1315@gmail.com>
2018-11-16 17:52:38 -0700

    Should get the data from the API

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
-import 'dart:_http';
+import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,22 +21,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Pokedex{
-  Pokedex() {
-    HttpClient client = new HttpClient();
-    client.getUrl(Uri.parse("https://pokeapi.co/api/v2/pokemon/"))
-        .then((HttpClientRequest request) {
-      // Optionally set up headers...
-      // Optionally write to the request object...
-      // Then call close.
-      return request.close();
-    })
-        .then((HttpClientResponse response) {
-      // Process the response.
-      if(response.statusCode == 200){
-        Pokedex.fromJson
-      }
-    });
-  }
+Future<Pokedex> fetchPokedex() async {
+  final response =
+  await http.get('https://pokeapi.co/api/v2/pokemon/');
 
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
+    return Pokedex.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load Pokedex');
+  }
+}
+
+class Pokedex {
+  final String url;
+  final String name;
+
+  Pokedex({this.name, this.url});
+
+  factory Pokedex.fromJson(Map<String, String> json) {
+    return Pokedex(
+      name: json["name"],
+      url: json["url"],
+    );
+  }
 }
